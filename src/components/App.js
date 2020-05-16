9c405467b586e25ed2560bdb45bff585d0d40444
Caleb335 <belac335@gmail.com>
2020-05-16 01:57:01 +0100

    fixed game logic

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ export default class App extends React.Component {
                     winner: this.determineWinner()
                 })
             }
-        }, 1000)
+        }, 180)
     }
 
     determineWinner() {
