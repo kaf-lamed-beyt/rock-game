95abc3ab3741afee7f57e015a85636e87858891b
Caleb335 <belac335@gmail.com>
2020-06-25 20:42:09 +0100

    refactor game speed

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,7 @@ export default class App extends React.Component {
                     winner: this.determineWinner(),
                 })
             }
-        }, 150)
+        }, 500)
     }
 
     determineWinner() {
